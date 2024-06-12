98a71c6c2ba7d581fb373d308aa99572f169322b
HalynaLysun <125215419+HalynaLysun@users.noreply.github.com>
2024-06-12 18:46:55 +0300

    all operations

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,18 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    let contacts = JSON.parse(data);
+    if (contacts.length !== 0) {
+      contacts = [];
+      await fs.writeFile(PATH_DB, JSON.stringify(contacts));
+      console.log(`Successful operation. Removed all contacts.`);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 await removeAllContacts();
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,18 +1,16 @@
 import { PATH_DB } from '../constants/contacts.js';
-import { createFakeContact } from '../utils/createFakeContact.js';
 import fs from 'fs/promises';
 
-export const addOneContact = async () => {
+export const removeLastContact = async () => {
   try {
     const data = await fs.readFile(PATH_DB, 'utf8');
     const contacts = JSON.parse(data);
-    contacts.push(createFakeContact());
-
+    contacts.pop();
     await fs.writeFile(PATH_DB, JSON.stringify(contacts));
-    console.log(`Successful operation. Added 1 contact.`);
+    console.log(`Successful operation. Removed last contact.`);
   } catch (err) {
     console.log(err);
   }
 };
 
-await addOneContact();
+await removeLastContact();
